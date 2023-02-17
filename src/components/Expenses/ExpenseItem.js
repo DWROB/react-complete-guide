9a41b450802d91a32eeb9fdf153f47c140266f4c
Dal Robinson <dalwrobinson@protonmail.com>
2023-02-17 10:18:51 +0000

    expenses filter added and expense form updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,24 +6,25 @@ import "./ExpenseItem.css";
 
 // components in react are just Js functions
 
+// item is a dumb component so have removed example that alter state.
+
 const ExpenseItem = (props) => {
   // useState always returns an array of two values.  The value and a function
   // for updating the value in 0.
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("updated");
-  };
+  // const clickHandler = () => {
+  //   setTitle("updated");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* event listener */}
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
